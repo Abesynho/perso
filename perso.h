1bf26d1439ec1807a7b0ce1d36be220b2df892d7
Abesynho <mouhamed@esprit.tn>
2021-03-15 21:07:04 +0100

    MAJ

--- perso.h
+++ perso.h
@@ -2,9 +2,19 @@
 #ifndef PERSO_H_
 #define PERSO_H_
 
+typedef struct 
+{
+int a;
+}Personne;
+
+
 
 void init (Personne * p, int numperso);
-
+void initPerso(Personne *p);
+void afficherPerso(Personne p, SDL_Surface * screen);
+void deplacerPerso (Personne *p);
+void animerPerso (Personne* p);
+void saut (Personne* p);
 
 #endif
 
